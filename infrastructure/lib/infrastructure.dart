025251835f20c3f6de0d97d294c065c5dae58ee5
Davide Zordan <mail@davide.dev>
2020-03-31 21:47:55 +0100

    Moving view_model_base to infrastructure package

---
+++ infrastructure/lib/infrastructure.dart
@@ -0,0 +1,3 @@
+library infrastructure;
+
+export 'ViewModels/view_model_base.dart';
---
+++ infrastructure/test/infrastructure_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:infrastructure/infrastructure.dart';
+
+void main() {
+  // test here.
+}
