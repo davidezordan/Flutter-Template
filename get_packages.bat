316e9a9ba00b4456bc5f962da7996fc46cd4c6db
Davide Zordan <mail@davide.dev>
2020-04-01 17:31:40 +0100

    Adding batch file for get_packages

---
+++ get_packages.bat
@@ -0,0 +1,20 @@
+cd .\ai_translator\app
+call flutter pub get
+
+cd ..\services
+call flutter pub get
+
+cd ..\ui
+call flutter pub get
+
+cd ..\..\common
+call flutter pub get
+
+cd ..\networking
+call flutter pub get
+
+cd ..\resources
+call flutter pub get
+
+cd ..\infrastructure
+call flutter pub get
